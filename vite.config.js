ba9144ccb61c7fa67a3e9892d6cdbf35113339cb
danish <danishjpiet@gmail.com>
2025-04-11 10:10:07 +0530

    added log

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: [''],
+    allowedHosts: ['instagram-frontend-coral.vercel.app'],
   },
 })
