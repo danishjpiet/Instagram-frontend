217b541e3217af861183a75decdaf45f384b4496
danish <danishjpiet@gmail.com>
2025-04-11 10:06:38 +0530

    added log

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
+console.log(API_BASE_URL);
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
